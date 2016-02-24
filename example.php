22d3201f281ceaad04045d4d5e7fd605e5aa00b2
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-02-24 15:48:45 +0000

    u

--- example.php
+++ example.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html>
-      <head>
-        </head>
-        <body>
-          <P>
-              <?php
-              echo "Hello World";
-              ?>
-          </P>
-       </body>
+<head>
+</head>
+<body>
+<P>
+    <?php
+    echo "Hello World";
+$myage=28;
+if ($myage >=21){
+    echo "You can buy specs,mugs, and sausage rolls";
+  }
+elseif ($myage >=18) {
+    echo "You can buy specs mugs";
+  }
+elseif ($myage >=16) {
+    echo "You can buy specs";
+  }
+else {
+   echo "You can`t buy anything";
+  }    ?>
+</P>
+</body>
 </html>
+
