b9eed4e797f19c26c2028a109376a9648edebb22
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-02 17:17:52 +0000

    u

--- login.php
+++ login.php
@@ -19,8 +19,7 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
     $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
 
     $result=mysqli_query($db,$sql);
-
-    if(mysqli_num_rows($result) == 1);
+    if(mysqli_num_rows($result) == 1)
     {
         header("location: home.php"); // Redirecting To another Page
     } else
