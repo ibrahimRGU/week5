eae8842f53fefd18cfab8f5bbfb8037a9c98771a
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-02 16:42:53 +0000

    u

---
+++ home.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516312
+ * Date: 02/03/2016
+ * Time: 16:40
+ */
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello</h1>
+</body>
+</html>
+
+?>
