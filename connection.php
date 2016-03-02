0fa6bb9fb68024f2a046278868dbd24970ad228e
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-02 17:25:42 +0000

    u

--- connection.php
+++ connection.php
@@ -5,9 +5,9 @@
  * Date: 02/03/2016
  * Time: 15:49
  */
-define('DB_SERVER', 'samsung1516312');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'bfd67c836b0287');
 define('DB_PASSWORD', '679c1a2e');
-define('DB_DATABASE', 'Database=samsung1516312;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=bfd67c836b0287;Password=679c1a2e');
+define('DB_DATABASE', 'samsung1516312');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
