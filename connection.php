21e422371615f793ea2954af22953fd263535e0e
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-02 16:17:17 +0000

    u

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1516312
+ * Date: 02/03/2016
+ * Time: 15:49
+ */
+define('DB_SERVER', 'samsung1516312');
+define('DB_USERNAME', 'bfd67c836b0287');
+define('DB_PASSWORD', '679c1a2e');
+define('DB_DATABASE', 'Database=samsung1516312;Data Source=ap-cdbr-azure-east-c.cloudapp.net;User Id=bfd67c836b0287;Password=679c1a2e');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
