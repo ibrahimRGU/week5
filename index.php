bf381d9e6f011e03fbf48517daf4dd85bf29aeb9
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-19 11:34:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,30 @@
 <?php
-	include('login.php'); // Include Login Script
+/**
+ * Created by PhpStorm.
+ * User: 1516312
+ * Date: 02/03/2016
+ * Time: 16:18
+ */
 
-	if ((isset($_SESSION['username']) != '')) 
-	{
-		header('Location: home.php');
-	}	
+include("connection.php"); //Establishing connection with our database*//
+
+if(empty($_POST["username"]) || empty($_POST["password"]))
+{
+    echo "Both fields are required.";
+}else
+{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $sql="SELECT uid FROM users WHERE username='$username' and password='$password'";
+    $result=mysqli_query($db,$sql);
+    if(mysqli_num_rows($result) == 1)
+    {
+        header("location: home.php"); // Redirecting To another Page
+    } else
+    {
+        echo "Incorrect username or password.";
+    }
+}
+
+
 ?>
-
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>PHP Login Form with Session</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-
-<body>
-<div align="center">
-<h1 style="font-family:Cambria, 'Hoefler Text', 'Liberation Serif', Times, 'Times New Roman', serif; font-size:32px;">PHP Login Form with Session</h1>
-<div class="loginBox">
-    <h3>Login Form</h3>
-    <br><br>
-    <form method="post" action="">
-        <label>Username:</label><br>
-        <input type="text" name="username" placeholder="username" /><br><br>
-        <label>Password:</label><br>
-        <input type="password" name="password" placeholder="password" />  <br><br>
-        <input type="submit" name="submit" value="Login" /> 
-    </form>
-    <div class="error"><?php echo $error;?></div>
-</div>
-</div>
-</body>
-</html>
