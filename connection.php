aa14a514a98ef40174f92c280e8a2bef3df90e1b
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-19 06:46:29 +0000

    Added files via upload

--- connection.php
+++ connection.php
@@ -1,9 +1,8 @@
 <?php
-
-define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME', 'bfd67c836b0287');
-define('DB_PASSWORD', '679c1a2e');
-define('DB_DATABASE', 'samsung1516312');
+define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
+define('DB_USERNAME', 'b99256e599d9fc');
+define('DB_PASSWORD', '64c23a04');
+define('DB_DATABASE', 'rgutest');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
