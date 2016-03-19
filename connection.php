c5c1699a2686d713f2461184f896c9e46b30ec9f
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-03-19 10:45:02 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,8 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
-define('DB_USERNAME', 'b99256e599d9fc');
-define('DB_PASSWORD', '64c23a04');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'bfd67c836b0287');
+define('DB_PASSWORD', '679c1a2e');
 define('DB_DATABASE', 'smasung1516312');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
-//// ($_SERVER ["eu-cdbr-azure-north-d.cloudapp.net"] .
-//"/smasung1516312"); 
-//$connection mysql_connect($db_host, $db_user, $db_password) or die ("error connecting")
