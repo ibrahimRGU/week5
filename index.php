4e1d07aa9f357fbf15a2550b828dbb941de838f1
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-02 12:25:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -46,14 +46,14 @@
 		<div align="center">
 	
 	<div class="loginBox">
-			<h3>Pleas Login Here</h3>
+			<h3>Please Login Here</h3>
 			<br><br>
 		<form method="post" action="">
 			<label>Username:</label><br>
 			<input type="text" name="username" placeholder="username" /><br><br>
 			<label>Password:</label><br>
 			<input type="password" name="password" placeholder="password" />  <br><br>
-			<input type="submit" name="submit" value="Login" /> 
+			<input type="submit" name="submit" value="Login" /> <br>
 			<a href="register.php" style="font-size:18px">Signup Here</a>
 		</form>
     <div class="error"><?php echo $error;?></div>
