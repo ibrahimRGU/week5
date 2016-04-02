4e8d62bca27ac3fe0768a841d80f9ae888426475
ibrahimRGU <i.m.abubakar@rgu.ac.uk>
2016-04-02 13:03:15 +0100

    Update home.php

--- home.php
+++ home.php
@@ -2,20 +2,44 @@
 	include("check.php");	
 	
 ?>
+<!DOCTYPE HTML>
+<Html>
+ 
 
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Home</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-<a href="logout.php" style="font-size:18px">Logout?</a>
-<body>
+	<head>
+	    <title>Course Work</title>
+		<meta charset="UTF-8">
+	</head>
+   	<link href="style.css" rel="stylesheet" type="text/css" />
+  <body>
+  <div class="main_container"> 
+	   <div class="welcome_container"> 
+	   <div class="welcome">
+	      <h1>Bug Tracking Site</h1>
+		  </div>  <!--close welcome-->
+		  </div>
+		<div class="slogan_container"> 
+	       <div class="welcome_slogan">   
+	      <h4>WE FIX BUGS</h4>
 
-
-
-<div class="conts">
+	       </div><!--close welcome_slogan-->
+		</div>
+		<div class="menu_container">
+		<div class="menu">
+          <li><a href="index.php">About US</a></li>
+          <li><a href="Contact.html">Contact Us</a></li>
+         <li><a href="logout.php" style="font-size:18px">Logout?</a></li>
+      </div><!--close menubar-->
+	  </div><!--close menubar_container-->
+		
+	  </div><!--Main_container-->
+	 
+		
+	<div class="new">
+		
+		<div class="textarea1"> 
+		<div align="center">
+	<div class="conts">
 <h1 class="hello">View Bug</h1>
 <?php
 	include("connection.php");
@@ -50,7 +74,26 @@
 
 </div>
 <br><br><br>
-
-</body>
-</html>
-
+	
+		</div>
+		</div>
+	<div class="sidebar"> 
+		<p><i>Usefull links</i></p>
+		<p>HTML</p>
+		<p>CSS</p>
+		<p>PHP</p>
+		<p>MySQL</p>	
+	</div>
+	</div> 
+	
+	<div class="footer">
+	<div footer="footer_content">
+		<footer>
+<address>&copy; Copyright 2016 All Rights Reserved east3.azurewebsites.net </address>
+</footer>
+	</div>
+	</div>
+	
+  </body>
+   
+</Html>
